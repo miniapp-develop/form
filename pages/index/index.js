8e2975f04646018de09738d5bb89d0f3f3bdc4c1
xesam <xesamguo@gmail.com>
2022-08-08 11:15:27 +0800

    from /fix

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,72 @@
 Page({
     data: {
         viewModel: {
-            name: 'xesam',
-            age: 22,
-            married: true
+            name: {
+                name: '姓名',
+                value: 'xpy',
+                format: {
+                    getValueView(value) {
+                        return value;
+                    },
+                    getViewValue(value) {
+                        return value;
+                    }
+                }
+            },
+            age: {
+                name: '年龄',
+                value: 22,
+                format: {
+                    getValueView(value) {
+                        return value;
+                    },
+                    getViewValue(value) {
+                        return value;
+                    }
+                }
+            },
+            type: {
+                name: '职位',
+                value: 1,
+                format: {
+                    getValueView(value) {
+                        return value === 0 ? '管理员' : '普通员工';
+                    },
+                    getViewValue(value) {
+                        return value;
+                    }
+                }
+            },
+            married: {
+                name: '婚姻状况',
+                value: true,
+                format: {
+                    getValueView(value) {
+                        return value ? '已婚' : '单身';
+                    },
+                    getViewValue(value) {
+                        return value;
+                    }
+                }
+            },
+            workingYears: {
+                name: '工作年限',
+                value: 100,
+                choices: [],
+                format: {
+                    getValueView(value) {
+                        return value ? '已婚' : '单身';
+                    },
+                    getViewValue(value) {
+                        return value;
+                    }
+                }
+            },
         },
-        getNumber: {
+        format: {
             getValue(value) {
-                return Number(value);
+                debugger
+                return value;
             }
         }
     },
